db6905afcb861773bbf7dee74537724260420eec
yarocruz <yarocruz@gmail.com>
2020-02-23 19:54:50 -0500

    added snippet to test out inquirer with fs

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 let fs = require('fs');
 let inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Title',
+        name: 'title'
+    }
+]).then(answers => {
+    fs.appendFile('README-test.md', `# ${answers.title}`, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    })
+})

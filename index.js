7474fd97ba5e4d095b0d13b1e365df4f865d74c5
yarocruz <yarocruz@gmail.com>
2020-02-25 21:23:29 -0500

    added initial questions to inquirer prompt

--- index.js
+++ index.js
@@ -5,12 +5,64 @@ inquirer.prompt([
     {
         type: 'input',
         message: 'Title',
-        name: 'title'
-    }
+        name: 'title',
+        default: 'Title'
+    },
+    {
+        type: 'input',
+        message: 'Description',
+        name: 'description',
+        default: 'Description'
+    },
+    {
+        type: 'input',
+        message: 'Table of Contents',
+        name: 'tableOfContents',
+        default: 'Table of Contents'
+    },
+    {
+        type: 'input',
+        message: 'Installation',
+        name: 'installation',
+        default: 'Installation'
+    },
+    {
+        type: 'input',
+        message: 'Usage',
+        name: 'usage',
+        default: 'Usage'
+    },
+    {
+        type: 'input',
+        message: 'License',
+        name: 'license',
+        default: 'MIT'
+    },
+    {
+        type: 'input',
+        message: 'Contributing',
+        name: 'contributing',
+        default: 'anonymous'
+    },
+    {
+        type: 'confirm',
+        message: 'Tests',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github users Name',
+        name: 'github',
+        default: 'username'
+    },
+
 ]).then(answers => {
-    fs.appendFile('README-test.md', `# ${answers.title}`, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-    })
+    let content = `# ${answers.title} \n## ${answers.description}\n## ${answers.tableOfContents}\n## ${answers.installation}\n## ${answers.usage}\n## ${answers.license}\n## ${answers.contributing}\n## ${answers.tests}\n## ${answers.github} `
+    fs.writeFile('README-test.md',
+        content,
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        })
 })
